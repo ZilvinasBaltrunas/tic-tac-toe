53d207816aaee878b97594e2a5dd4ef5bff2785c
ZilvinasBaltrunas <zilvino@hotmail.com>
2018-08-09 20:20:45 +0300

    added basic box click handler & couple basic utility functions

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-(() => {
+(function IIFE() {
 	const gameData = [...Array(3)].map(item => [...Array(3)]);
 	const app = document.querySelector('#app');
+	const cross = '🍏';
+	const circle = '🍎';
+
+	const create = ({ tag, classList, textContent, events = {} }) => {
+		const element = document.createElement(tag);
+		element.classList = classList;
+		element.textContent = textContent;
+		Object.entries(events).forEach(([key, value]) => 
+			element.addEventListener(key, value)
+		);
+
+		return element;
+	};
+
+	const addGameData = (row, box) => {
+		gameData[row][box] = cross;
+		render();
+	};
+
+	const clearApp = () => {
+		const children = [...app.children];
+		children.forEach(child => app.removeChild(child));
+	};
 
 	const render = () => {
-		gameData.forEach(rowData => {
-			const row = document.createElement('div');
-			row.classList.add('row');
-			rowData.forEach(boxData => {
-				const box = document.createElement('div');
-				box.classList.add('box');
-				box.textContent = boxData;
+		clearApp();
+		gameData.forEach((rowData, rowIndex) => {
+			const row = create({tag: 'div', classList: 'row'});
+
+			rowData.forEach((boxData, boxIndex) => {
+				const box = create({
+					tag: 'div',
+					classList: 'box',
+					textContent: boxData,
+					events: { click: () => addGameData(rowIndex, boxIndex) },
+				});
 				row.appendChild(box);
 			});
-
 			app.appendChild(row);
 		});
 	};
 
-	const start = ({ target: element }) => {
-		console.log('element', element);
-		app.removeChild(element);
+	const start = ({ target }) => {
+		clearApp();
 		render();
 	};
 
 	const initApp = () => {
-		const startButton = document.createElement('button');
-		startButton.textContent = 'Start';
-		startButton.classList.add('primary-button');
-		startButton.addEventListener('click', start);
+		const startButton = create({
+			tag: 'button', 
+			classList: 'primary-button', 
+			textContent: 'Start', 
+			events: {click: start},
+		});
+		
 		app.appendChild(startButton);
 	};
 
